0ba591a8a7e635d4eb6cad5c9a2f613c8b0d62f5
LYChubby <f4uzi345@gmail.com>
2025-05-06 16:25:37 +0700

    middleware, gate admin

--- routes/web.php
+++ routes/web.php
@@ -24,18 +24,21 @@ Route::get('/todo/create', [TodoController::class, 'create']) -> name ('todo.cre
 Route::get('/todo/edit', [TodoController::class, 'edit']) -> name ('todo.edit');
 Route::get('/todo/{todo}/edit', [TodoController::class, 'edit']) -> name ('todo.edit');
 
-Route::get('/user', [UserController::class, 'index']) -> name ('user.index');
-
 Route::patch('/todo/{todo}/complete', [TodoController::class, 'complete']) -> name ('todo.complete');
 Route::patch('/todo/{todo}/uncomplete', [TodoController::class, 'uncomplete']) -> name ('todo.uncomplete');
-Route::patch('/user/{user}/makeadmin', [UserController::class, 'makeadmin']) -> name ('user.makeadmin');
-Route::patch('/user/{user}/removeadmin', [UserController::class, 'removeadmin']) -> name ('user.removeadmin');
 Route::patch('/todo/{todo}', [TodoController::class, 'update']) -> name ('todo.update');
-
 Route::delete('/todo/{todo}', [TodoController::class, 'destroy']) -> name ('todo.destroy');
 Route::delete('/todo', [TodoController::class, 'destroyCompleted']) -> name ('todo.deleteallcompleted');
-Route::delete('/user/{user}', [UserController::class, 'destroy']) -> name ('user.destroy');
 
 Route::resource('todo', TodoController::class)->except(['show']);
+
+Route::get('/user', [UserController::class, 'index']) -> name ('user.index');
+
+Route::middleware(['auth', 'admin'])->group(function () {
+    Route::resource('user', UserController::class)->except(['show']);
+    Route::patch('/user/{user}/makeadmin', [UserController::class, 'makeadmin']) -> name ('user.makeadmin');
+    Route::patch('/user/{user}/removeadmin', [UserController::class, 'removeadmin']) -> name ('user.removeadmin');
+    Route::delete('/user/{user}', [UserController::class, 'destroy']) -> name ('user.destroy');
+});
 
 require __DIR__.'/auth.php';
